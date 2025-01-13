054ce45f1c66cfb67449505f5ed777cb2745719b
akatgelar <akatgelar@gmail.com>
2025-01-13 10:49:05 +0700

    feat: model user

--- app/models/all_models.py
+++ app/models/all_models.py
@@ -1,4 +1,5 @@
  
 from app.models.content_model import ContentModel
+from app.models.user_model import UserModel
 
-all_models = [ContentModel]
+all_models = [ContentModel, UserModel]
---
+++ app/models/user_model.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+from app.config.database import Base
+
+class UserModel(Base):
+    __tablename__ = 'users' 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    fullname: Mapped[str] = mapped_column(nullable=True)
+    role: Mapped[str] = mapped_column(nullable=True)
+    is_active: Mapped[bool] = mapped_column(default=False)
+    created_by: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now(ZoneInfo('Asia/Jakarta')))
+    updated_by: Mapped[int] = mapped_column(nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(ZoneInfo('Asia/Jakarta')))
+
+class UserSchema(BaseModel): 
+    username: str
+    password: str
+    fullname: str
+    role: str 
+    is_active: bool
+    created_by: int
+    created_at: datetime
+    updated_by: int
+    updated_at: datetime
+ 
