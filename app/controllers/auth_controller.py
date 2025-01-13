6d6c43805c1d3dce78b24721594483e8047d234b
akatgelar <akatgelar@gmail.com>
2025-01-13 11:50:12 +0700

    feat: jwt token handle

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,5 +1,5 @@
 #pylint: disable = no-self-argument, no-member
-from fastapi import Depends, HTTPException, status
+from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.models.user_model import UserModel
@@ -11,14 +11,13 @@ class AuthController(object):
         try:
             datas = db.query(UserModel).filter(UserModel.username == auth_data.username).first()
             if not datas:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"User tidak ditemukan")
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User tidak ditemukan")
             
             if not Helper.verify_password(auth_data.password, datas.password):
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Password salah")
             
             del datas.password
-            access_token = Helper.create_access_token(data=datas, expires_delta=None)
-            
+            access_token = Helper.create_access_token(data=datas, expires_delta=None) 
             result = {"access token" : access_token, "token_type": "bearer"}
             return result
         except Exception as e:
